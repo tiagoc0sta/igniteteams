306b2bdf371880feb0a9b6300e534cbfb0dc9fdc
Tiago Costa <tiagodpv@gmail.com>
2023-04-10 19:51:24 -0400

    edit index

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,9 +2,12 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
+import { useState } from 'react';
 //import * as S from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState();
+
   return (
     <Container>
       <Header />
