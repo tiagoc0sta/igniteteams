f6cab6ce8c478ee99b295088cfa5eaa7ce2ee77c
Tiago Costa <tiagodpv@gmail.com>
2023-04-10 20:50:19 -0400

    add error

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,10 +3,11 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 import { useState } from 'react';
+import { FlatList } from 'react-native/types';
 //import * as S from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState();
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
 
   return (
     <Container>
