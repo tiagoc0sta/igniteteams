5ec79ac8fc928c34628dd3c46c02f102be076d65
Tiago Costa <tiagodpv@gmail.com>
2023-04-11 19:37:03 -0400

    add list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,18 +3,26 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 import { useState } from 'react';
-import { FlatList } from 'react-native/types';
+import { FlatList } from 'react-native';
 //import * as S from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([
+    'Galera da Rocket',
+    'Amigos',
+    'Família',
+  ]);
 
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='joque com sua turma' />
 
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
